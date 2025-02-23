1f558c6c18038dc0e85955df82db406c81f77f0e
Ruan Ribeiro <ruan.pablo.pec@hotmail.com>
2025-02-23 19:55:56 -0300

    add prometheus and grafana in a docker compose

--- app/cmd/api/api.go
+++ app/cmd/api/api.go
@@ -12,6 +12,9 @@ import (
 	"github.com/hoyci/book-store-api/service/healthcheck"
 	"github.com/hoyci/book-store-api/service/user"
 	"github.com/hoyci/book-store-api/utils"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
@@ -39,37 +42,128 @@ func (s *APIServer) SetupRouter(
 ) *mux.Router {
 	utils.InitLogger()
 	router := mux.NewRouter()
+	registry := prometheus.NewRegistry()
+	metricsMiddleware := utils.New(registry, nil)
+
+	registry.MustRegister(
+		collectors.NewGoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+	)
 
 	router.Use(utils.LoggingMiddleware)
 
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
-	subrouter.HandleFunc("/swagger.json", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "docs/swagger.json")
-	})
+	subrouter.HandleFunc(
+		"/metrics",
+		metricsMiddleware.WrapHandler(
+			"metrics",
+			promhttp.HandlerFor(
+				registry,
+				promhttp.HandlerOpts{},
+			),
+		),
+	)
 
-	subrouter.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:8080/api/v1/swagger.json"),
-		httpSwagger.DeepLinking(true),
-		httpSwagger.DocExpansion("none"),
-		httpSwagger.DomID("swagger-ui"),
-	)).Methods(http.MethodGet)
+	subrouter.HandleFunc(
+		"/swagger.json",
+		metricsMiddleware.WrapHandler(
+			"swagget_json",
+			http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					http.ServeFile(w, r, "docs/swagger.json")
+				},
+			),
+		),
+	)
 
-	subrouter.HandleFunc("/healthcheck", healthCheckHandler.HandleHealthCheck).Methods(http.MethodGet)
+	subrouter.PathPrefix("/swagger/").Handler(
+		metricsMiddleware.WrapHandler(
+			"swagget_ui",
+			httpSwagger.Handler(
+				httpSwagger.URL("http://localhost:8080/api/v1/swagger.json"),
+				httpSwagger.DeepLinking(true),
+				httpSwagger.DocExpansion("none"),
+				httpSwagger.DomID("swagger-ui"),
+			),
+		),
+	).Methods(http.MethodGet)
 
-	subrouter.HandleFunc("/auth", authHandler.HandleUserLogin).Methods(http.MethodPost)
-	subrouter.HandleFunc("/auth/refresh", authHandler.HandleRefreshToken).Methods(http.MethodPost)
+	subrouter.HandleFunc(
+		"/healthcheck",
+		metricsMiddleware.WrapHandler("healthcheck", http.HandlerFunc(healthCheckHandler.HandleHealthCheck)),
+	).Methods(http.MethodGet)
 
-	subrouter.Handle("/books", utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleCreateBook))).Methods(http.MethodPost)
-	subrouter.Handle("/books/{id}", utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleGetBookByID))).Methods(http.MethodGet)
-	subrouter.Handle("/books", utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleGetBooks))).Methods(http.MethodGet)
-	subrouter.Handle("/books/{id}", utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleUpdateBookByID))).Methods(http.MethodPut)
-	subrouter.Handle("/books/{id}", utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleDeleteBookByID))).Methods(http.MethodDelete)
+	subrouter.HandleFunc(
+		"/auth",
+		metricsMiddleware.WrapHandler("auth", http.HandlerFunc(authHandler.HandleUserLogin)),
+	).Methods(http.MethodPost)
+	subrouter.HandleFunc(
+		"/auth/refresh",
+		metricsMiddleware.WrapHandler("auth/refresh", http.HandlerFunc(authHandler.HandleRefreshToken)),
+	).Methods(http.MethodPost)
 
-	subrouter.HandleFunc("/users", userHandler.HandleCreateUser).Methods(http.MethodPost)
-	subrouter.Handle("/users", utils.AuthMiddleware(http.HandlerFunc(userHandler.HandleGetUserByID))).Methods(http.MethodGet)
-	subrouter.Handle("/users", utils.AuthMiddleware(http.HandlerFunc(userHandler.HandleUpdateUserByID))).Methods(http.MethodPut)
-	subrouter.Handle("/users", utils.AuthMiddleware(http.HandlerFunc(userHandler.HandleDeleteUserByID))).Methods(http.MethodDelete)
+	subrouter.HandleFunc(
+		"/users",
+		metricsMiddleware.WrapHandler("create_user", http.HandlerFunc(userHandler.HandleCreateUser)),
+	).Methods(http.MethodPost)
+	subrouter.Handle(
+		"/users",
+		metricsMiddleware.WrapHandler(
+			"get_user",
+			utils.AuthMiddleware(http.HandlerFunc(userHandler.HandleGetUserByID)),
+		),
+	).Methods(http.MethodGet)
+	subrouter.Handle(
+		"/users",
+		metricsMiddleware.WrapHandler(
+			"update_user",
+			utils.AuthMiddleware(http.HandlerFunc(userHandler.HandleUpdateUserByID)),
+		),
+	).Methods(http.MethodPut)
+	subrouter.Handle(
+		"/users",
+		metricsMiddleware.WrapHandler(
+			"delete_user",
+			utils.AuthMiddleware(http.HandlerFunc(userHandler.HandleDeleteUserByID)),
+		),
+	).Methods(http.MethodDelete)
+
+	subrouter.Handle(
+		"/books",
+		metricsMiddleware.WrapHandler(
+			"create_book",
+			utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleCreateBook)),
+		),
+	).Methods(http.MethodPost)
+	subrouter.Handle(
+		"/books/{id}",
+		metricsMiddleware.WrapHandler(
+			"get_book_by_id",
+			utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleGetBookByID)),
+		),
+	).Methods(http.MethodGet)
+	subrouter.Handle(
+		"/books",
+		metricsMiddleware.WrapHandler(
+			"get_books",
+			utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleGetBooks)),
+		),
+	).Methods(http.MethodGet)
+	subrouter.Handle(
+		"/books/{id}",
+		metricsMiddleware.WrapHandler(
+			"update_book_by_id",
+			utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleUpdateBookByID)),
+		),
+	).Methods(http.MethodPut)
+	subrouter.Handle(
+		"/books/{id}",
+		metricsMiddleware.WrapHandler(
+			"delete_book_by_id",
+			utils.AuthMiddleware(http.HandlerFunc(bookHandler.HandleDeleteBookByID)),
+		),
+	).Methods(http.MethodDelete)
 
 	s.Router = router
 
